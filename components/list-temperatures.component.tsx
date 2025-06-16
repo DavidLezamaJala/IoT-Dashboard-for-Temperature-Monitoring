1e35e617d07e0c0f29fef0e51296c8900282c415
David Lezama <david.lezama@jala.university>
2025-06-15 21:59:22 -0600

    fix(list-temperatures): display raw time value instead of formatted time

--- components/list-temperatures.component.tsx
+++ components/list-temperatures.component.tsx
@@ -67,7 +67,7 @@ export const ListTemperatures = ({ temperatures }: {
                       d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
                     />
                   </svg>
-                  {new Date(time).toLocaleTimeString()}
+                  {time}
                 </div>
               </div>
             </div>
